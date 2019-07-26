6f37215fdbbd99a06cec678ccdcd58e62c4fe3e3
farp80 <farp80@gmail.com>
2019-07-26 00:16:20 +0000

    fixed All class to className

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,12 +3,14 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./navbar";
+import { Jumbotron } from "./jumbotron";
 //create your first component
 export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
 				<Navbar />
+				<Jumbotron />
 				<h1>Hello Rigo!</h1>
 				<p>
 					<img src={rigoImage} />
--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -1,12 +1,20 @@
 import React from "react";
 export const Jumbotron = () => {
-    return (
-        <div className="container">
-            <header className="jumbotron my-4">
-               <h1 className="display-3">Peer Programming</h1>
-               <p className="lead">This concept should be applied in Companies. Having 4 eyes instead of 2 is the best advantage. Driver and Navigator is the another recent concept applied to Google.</p>
-               <a href="https://en.wikipedia.org/wiki/Pair_programming" className="btn btn-primary btn-lg">Concept</a>
-           </header>
-        </div>
-    );
+	return (
+		<div className="container">
+			<header className="jumbotron my-4">
+				<h1 className="display-3">Peer Programming</h1>
+				<p className="lead">
+					This concept should be applied in Companies. Having 4 eyes
+					instead of 2 is the best advantage. Driver and Navigator is
+					the another recent concept applied to Google.
+				</p>
+				<a
+					href="https://en.wikipedia.org/wiki/Pair_programming"
+					className="btn btn-primary btn-lg">
+					Concept
+				</a>
+			</header>
+		</div>
+	);
 };
