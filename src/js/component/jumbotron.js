fee92e5fbfda97165de62307df30deb712dbad0d
farp80 <farp80@gmail.com>
2019-07-26 00:11:02 +0000

    initialWork_2

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -3,9 +3,9 @@ export const Jumbotron = () => {
     return (
         <div className="container">
             <header className="jumbotron my-4">
-               <h1 className="display-3">A Warm Welcome!</h1>
-               <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-               <a href="#" className="btn btn-primary btn-lg">Call to action!</a>
+               <h1 className="display-3">Peer Programming</h1>
+               <p className="lead">This concept should be applied in Companies. Having 4 eyes instead of 2 is the best advantage. Driver and Navigator is the another recent concept applied to Google.</p>
+               <a href="https://en.wikipedia.org/wiki/Pair_programming" className="btn btn-primary btn-lg">Concept</a>
            </header>
         </div>
     );
